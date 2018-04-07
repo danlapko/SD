143de5cd7331d8236fdd4b1e75d725f0d6809aa6
Nikolay Kuzivanov <nicorab@mail.ru>
2018-04-07 22:03:41 +0300

    add comments

--- CLI/cli.py
+++ CLI/cli.py
@@ -4,34 +4,57 @@ import subprocess
 
 
 class Lexer:
+    '''
+    You should first launch instance of lexer on inputString
+    from console to proceed all occurrences of variables from environment.
+    '''
     def __init__(self):
         self.defaultInitilizate()
 
     def show(self):
+        '''
+        :return: result of last processed string by this instance
+        '''
         return self.lexerOut
 
     def defaultInitilizate(self):
+        '''
+        I am not sure that this method have right to exist but anyway.
+        Set default state for instance of Lexer
+        '''
         self.lexerOut = ""
         self.skipMode = False
         self.replaceMode = False
         self.curNameOfVariables = ""
 
     def launch(self, userInput):
+        '''
+        Take string and replace all occurrences of $name.
+        name could include alphabet symbols, numbers, all kind of
+        special symbols like !@#$%^&*())_+. Separator for name are
+        space, ', '', $
+        :param userInput: String
+        :return: String
+        '''
         self.defaultInitilizate()
         for character in userInput:
-            if self.skipMode:
+            if self.skipMode:  # just skip everything inside of ' '
                 if character == '\'':
                     self.skipMode = False
 
                 self.lexerOut += character
                 continue
             def dump():
+                '''
+                Service function. Change variable on it value.
+                '''
                 if self.curNameOfVariables in variabls:
                     self.lexerOut += variabls[self.curNameOfVariables]
                 self.curNameOfVariables = ""
                 self.replaceMode = False
 
             if not character.isspace():
+                # get name of variable until next separator
                 if self.replaceMode:
                     if character == '$':
                         dump()
@@ -64,6 +87,9 @@ class Lexer:
 
 
 class Token:
+    '''
+    Abstarct class for diagram
+    '''
     pass
 
 class Arguments(Token): # not used but might be in next version
@@ -74,6 +100,10 @@ class Parametrs(Token): # not used but might be in next version
 
 
 class Command(Token):
+    '''
+    Abstract class for commands
+    All commands use string as interface for communication
+    '''
     def __init__(self):
         self.argsFromInput = []
         self.paramsFromInput = []
@@ -82,15 +112,17 @@ class Command(Token):
         self.listOfSupportedParam = {}
 
     def check(self):
+        '''
+        command check its parameters before launch
+        '''
         if len(self.argsFromInput) > self.maxNumberOfArgs or len(self.argsFromInput) < self.minNumberOfArgs:
             raise Exception("Unsupported numbers of args for command {}".format(self.__class__.__name__))
         for param in self.paramsFromInput:
             if param not in self.listOfSupportedParam:
                 raise Exception("Unsupported params for command {}".format(self.__class__.__name__))
 
-
     def run(self):
-        raise Exception("Unimlemented Command")
+        raise Exception("Unimplemented Command")
 
 
 class Exit(Command):
@@ -107,6 +139,9 @@ class Exit(Command):
 
 
 class Cat(Command):
+    '''
+    Cat can only open files by relative name from current directory
+    '''
     def __init__(self):
         super(Cat, self).__init__()
 
@@ -115,7 +150,7 @@ class Cat(Command):
         numberOfRow = 0
         outputString = ""
         for fileName in self.argsFromInput:
-            file_in = open(fileName, "r")
+            file_in = open(curDir+"/"+fileName, "r")
             for line in file_in.read().splitlines():
                 numberOfRow = numberOfRow + 1
                 outputString += '{0}. {1}\n'.format(numberOfRow, line)
@@ -124,6 +159,9 @@ class Cat(Command):
 
 
 class Wc(Command):
+    '''
+    Take string for evaluations
+    '''
     def __init__(self):
         super(Wc, self).__init__()
 
@@ -144,6 +182,9 @@ class Wc(Command):
 
 
 class Echo(Command):
+    '''
+    Just remove '' and ""
+    '''
     def run(self):
         self.check()
         output = ""
@@ -153,6 +194,7 @@ class Echo(Command):
         output += "\n"
         return output
 
+
 class Pwd(Command):
     def __init__(self):
         super(Pwd, self).__init__()
@@ -161,10 +203,13 @@ class Pwd(Command):
 
     def run(self):
         self.check()
-        return curDir
+        return curDir + "\n"
 
 
 class Assignment(Command):
+    '''
+    Always execute first.
+    '''
     def __init__(self):
         super(Assignment, self).__init__()
         self.minNumberOfArgs = 2
@@ -175,7 +220,23 @@ class Assignment(Command):
         variabls[self.argsFromInput[0]] = self.argsFromInput[1]
 
 
+class Others(Command):
+    '''
+    try subprocess for every not implemented comand
+    '''
+    def run(self):
+        try:
+            unknownOut = subprocess.check_output(self.argsFromInput)
+        except:
+            raise Exception("Error in unknown command")
+        return unknownOut.decode()
+
+
 class Buffer:
+    '''
+    Class exist for purpose of polymorphism.
+    Parser use it to keep some word.
+    '''
     def __init__(self):
         self.value = ""
 
@@ -193,13 +254,25 @@ class Buffer:
 
 
 class Parser:
+    '''
+    parser going to work after lexer just to
+    split input string on sequence of commands
+    bounded by pipeline
+    '''
     def __init__(self):
         self.buf = Buffer()
         self.parserOut = []
 
     def launch(self, inputString):
+        '''
+        First phase: split by pipeline with consider of '' and ""
+        Second phase: create commands from array of strings
+        :param inputString:String
+        :return: array of commands
+        '''
         self.parserOut = []
         stringsSplitByPipe = []
+        #flag for consideration of ''
         skip = False
 
         for char in inputString:
@@ -217,9 +290,12 @@ class Parser:
         for each in stringsSplitByPipe:
             words = []
             self.buf.clear()
-            runnable = True
-            openSpace = False
+            runnable = True #if sequnce insade of '' it shold not interpritate
+            openSpace = False #considering sequnce insade "" and ''
             def dump(isRunable):
+                '''
+                Just to eliminate extra code copy/paste
+                '''
                 words.append((isRunable, self.buf.get()))
                 self.buf.clear()
 
@@ -262,15 +338,27 @@ class Parser:
                 if words[0][0] == 0:
                     raise Exception("Syntax error")
                 if words[0][1] not in listOfCommands:
-                    raise Exception("Unsupported command")
-                command = listOfCommands[words[0][1]]()
-                for id in range(1, len(words)):
-                    command.argsFromInput.append(words[id][1])
-                self.parserOut.append(command)
+                    unknownCommand = Others()
+                    for word in words:
+                        unknownCommand.argsFromInput.append(word[1])
+                    self.parserOut.append(unknownCommand)
+                else:
+                    command = listOfCommands[words[0][1]]()
+                    for id in range(1, len(words)):
+                        command.argsFromInput.append(words[id][1])
+                    self.parserOut.append(command)
 
 
 class Executor:
+    '''
+    Run commands one by one. Also transfer output from one to input other.
+    After all commands print result of their work.
+    '''
     def launch(self, comandsToRun):
+        '''
+        :param comandsToRun: array for Commands
+        :return:
+        '''
         result = None
         for comand in comandsToRun:
             if result is not None:
@@ -278,6 +366,7 @@ class Executor:
             result = comand.run()
         if result is not None:
             print(result, end='')
+
 
 curDir = os.getcwd()
 listOfCommands = {"exit":Exit, "wc":Wc, "cat":Cat, "echo":Echo, "pwd":Pwd, "=":Assignment}
